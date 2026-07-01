a8ad0dddce75484216c548e2d2c83993988ae6c1
Ayushi Patel <ayushipatel0409@gmail.com>
2026-07-02 04:03:29 +0530

    Added variable declarations and console.table

--- JavaScript/01_variables.js
+++ JavaScript/01_variables.js
@@ -1,1 +1,13 @@
 console.log("Hello Ayushi!!!");
+const AccountNumber = 234564
+let AccountId = "Jhalak@0409"
+var AccountType = "54675"
+AccountCity = "Lucknow"
+console.log(AccountId)
+console.log(AccountNumber)
+console.log(AccountType)
+console.log(AccountCity)
+AccountId = "Aryan1504"
+AccountType = "43574"
+AccountCity ="New York City"
+console.table([AccountId,AccountNumber,AccountType,AccountCity])
