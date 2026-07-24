ef9e80f58c0fcdf01a8d548065415d66f59f9b97
Ayushi Patel <ayushipatel0409@gmail.com>
2026-07-24 23:22:12 +0530

    feat: add arrays in JavaScript

---
+++ JavaScript/06_array.js
@@ -0,0 +1,76 @@
+//Array:
+const myArr = [0,1,2,3,4,5];
+// const myHeroes = ["Thor","Iron Man"];
+// const myAr = new Array(1,2,3,4);
+// console.log(myArr[1]);
+
+//Array Methods:
+// myArr.push(8);
+// myArr.push(6);
+// myArr.pop();
+
+// console.log(myArr);
+
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(5));
+
+// const newarr = myArr.join();
+// console.log(myArr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+//Slice ,Splice:
+
+// console.log("A" , myArr);
+// const myn1 = myArr.slice(1,3);
+
+// console.log(myn1);
+// console.log("B" , myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log("C",myArr);
+// console.log(myn2);
+
+
+//Array 2:
+
+const marvel = ["Thor","Ironman","spiderman"];
+const dc = ["superman","flash","batman"]
+
+// marvel.push(dc);
+
+// console.log(marvel);
+// console.log(marvel[3][1]);
+
+// const allhero = marvel.concat(dc);
+// console.log(allhero);
+
+// const allnew = [...marvel,...dc];
+// console.log(allnew);
+
+const another = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const real = another.flat(Infinity);
+// console.log(real);
+
+// console.log(Array.isArray("Ayushi"));
+// console.log(Array.from("Ayushi"));
+
+const s1 = 100;
+const s2 = 200;
+const s3 = 300;
+
+// console.log(Array.of(s1,s2,s3));
+
+
+
+
+
+
+
+
+
+
